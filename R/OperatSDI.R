44769b4e6b9b9c27e5c49ec76d82076bb8eea9d0
Adam H. Sparks <adamhsparks@icloud.com>
2023-09-25 13:07:44 +0800

    reformat code

--- R/OperatSDI.R
+++ R/OperatSDI.R
@@ -231,24 +231,30 @@ OperatSDI <-
     start.month <- as.numeric(format(start.date.user, format = "%m"))
     message("Calculating...")
     if (PEMethod == "HS") {
-      sse_i <- as.data.frame(get_power(community = "ag",
-                                       lonlat = c(lon, lat), dates = c(start.date.user,
-                                                                       end.date.user), temporal_api = "daily", pars = c("T2M",
-                                                                                                                        "T2M_MAX", "T2M_MIN", "PRECTOTCORR")))
-      decli <- 23.45 * sin((360 * (sse_i$DOY - 80)/365) *
-                             (pi/180))
-      lat.rad <- lat * (pi/180)
-      decli.rad <- decli * (pi/180)
+      sse_i <- as.data.frame(get_power(
+        community = "ag",
+        lonlat = c(lon, lat),
+        dates = c(start.date.user,
+                  end.date.user),
+        temporal_api = "daily",
+        pars = c("T2M",
+                 "T2M_MAX", "T2M_MIN", "PRECTOTCORR")
+      ))
+      decli <- 23.45 * sin((360 * (sse_i$DOY - 80) / 365) *
+                             (pi / 180))
+      lat.rad <- lat * (pi / 180)
+      decli.rad <- decli * (pi / 180)
       hn.rad <- (acos(tan(decli.rad) * -tan(lat.rad)))
-      hn.deg <- hn.rad * (180/pi)
-      N <- (2 * hn.deg)/15
-      dist.terra.sol <- 1 + (0.033 * cos((pi/180) * (sse_i$DOY *
-                                                       (360/365))))
-      Ra <- (37.6 * (dist.terra.sol^2)) * ((pi/180) *
-                                             hn.deg * sin(lat.rad) * sin(decli.rad) + (cos(lat.rad) *
-                                                                                         cos(decli.rad) * sin(hn.rad)))
+      hn.deg <- hn.rad * (180 / pi)
+      N <- (2 * hn.deg) / 15
+      dist.terra.sol <- 1 + (0.033 * cos((pi / 180) * (sse_i$DOY *
+                                                         (360 / 365))))
+      Ra <- (37.6 * (dist.terra.sol ^ 2)) * ((pi / 180) *
+                                               hn.deg * sin(lat.rad) * sin(decli.rad) + (cos(lat.rad) *
+                                                                                           cos(decli.rad) * sin(hn.rad)))
       ETP.harg.daily <- 0.0023 * (Ra * 0.4081633) * (sse_i$T2M_MAX -
-                                                       sse_i$T2M_MIN)^0.5 * (sse_i$T2M + 17.8)
+                                                       sse_i$T2M_MIN) ^
+        0.5 * (sse_i$T2M + 17.8)
       sse_i <- cbind(sse_i, ETP.harg.daily)
       n.tot <- length(sse_i[, 1])
       final.year <- sse_i$YEAR[n.tot]
@@ -299,36 +305,49 @@ OperatSDI <-
       }
     }
     if (PEMethod == "PM") {
-      sse_i <- as.data.frame(get_power(community = "ag",
-                                       lonlat = c(lon, lat), dates = c(start.date.user,
-                                                                       end.date.user), temporal_api = "daily", pars = c("T2M",
-                                                                                                                        "T2M_MAX", "T2M_MIN", "ALLSKY_SFC_SW_DWN",
-                                                                                                                        "WS2M", "RH2M", "PRECTOTCORR")))
-      decli <- 23.45 * sin((360 * (sse_i$DOY - 80)/365) *
-                             (pi/180))
-      lat.rad <- lat * (pi/180)
-      decli.rad <- decli * (pi/180)
+      sse_i <- as.data.frame(get_power(
+        community = "ag",
+        lonlat = c(lon, lat),
+        dates = c(start.date.user,
+                  end.date.user),
+        temporal_api = "daily",
+        pars = c(
+          "T2M",
+          "T2M_MAX",
+          "T2M_MIN",
+          "ALLSKY_SFC_SW_DWN",
+          "WS2M",
+          "RH2M",
+          "PRECTOTCORR"
+        )
+      ))
+      decli <- 23.45 * sin((360 * (sse_i$DOY - 80) / 365) *
+                             (pi / 180))
+      lat.rad <- lat * (pi / 180)
+      decli.rad <- decli * (pi / 180)
       hn.rad <- (acos(tan(decli.rad) * -tan(lat.rad)))
-      hn.deg <- hn.rad * (180/pi)
-      N <- (2 * hn.deg)/15
-      dist.terra.sol <- 1 + (0.033 * cos((pi/180) * (sse_i$DOY *
-                                                       (360/365))))
-      Ra <- (37.6 * (dist.terra.sol^2)) * ((pi/180) *
-                                             hn.deg * sin(lat.rad) * sin(decli.rad) + (cos(lat.rad) *
-                                                                                         cos(decli.rad) * sin(hn.rad)))
-      es <- 0.6108 * exp((17.27 * sse_i$T2M)/(sse_i$T2M +
-                                                273.3))
-      ea <- (sse_i$RH2M * es)/100
-      slope.pressure <- (4098 * es)/((sse_i$T2M + 237.3)^2)
+      hn.deg <- hn.rad * (180 / pi)
+      N <- (2 * hn.deg) / 15
+      dist.terra.sol <- 1 + (0.033 * cos((pi / 180) * (sse_i$DOY *
+                                                         (360 / 365))))
+      Ra <- (37.6 * (dist.terra.sol ^ 2)) * ((pi / 180) *
+                                               hn.deg * sin(lat.rad) * sin(decli.rad) + (cos(lat.rad) *
+                                                                                           cos(decli.rad) * sin(hn.rad)))
+      es <- 0.6108 * exp((17.27 * sse_i$T2M) / (sse_i$T2M +
+                                                  273.3))
+      ea <- (sse_i$RH2M * es) / 100
+      slope.pressure <- (4098 * es) / ((sse_i$T2M + 237.3) ^ 2)
       Q0.ajust <- 0.75 * Ra
       Rn <- (1 - 0.2) * sse_i$ALLSKY_SFC_SW_DWN - (1.35 *
-                                                     (sse_i$ALLSKY_SFC_SW_DWN/Q0.ajust) - 0.35) *
-        (0.35 - (0.14 * sqrt(ea))) * (5.67 * 10^-8) *
-        (((sse_i$T2M^4) + (sse_i$T2M_MIN^4))/2)
+                                                     (sse_i$ALLSKY_SFC_SW_DWN /
+                                                        Q0.ajust) - 0.35) *
+        (0.35 - (0.14 * sqrt(ea))) * (5.67 * 10 ^ -8) *
+        (((sse_i$T2M ^ 4) + (sse_i$T2M_MIN ^ 4)) / 2)
       ETP.pm.daily <- (0.408 * slope.pressure * (Rn -
-                                                   0.8) + 0.063 * (900/(sse_i$T2M + 273)) * sse_i$WS2M *
-                         (es - ea))/(slope.pressure + 0.063 * (1 + 0.34 *
-                                                                 sse_i$WS2M))
+                                                   0.8) + 0.063 * (900 /
+                                                                     (sse_i$T2M + 273)) * sse_i$WS2M *
+                         (es - ea)) / (slope.pressure + 0.063 * (1 + 0.34 *
+                                                                   sse_i$WS2M))
       sse_i <- cbind(sse_i, ETP.pm.daily)
       n.tot <- length(sse_i[, 1])
       final.year <- sse_i$YEAR[n.tot]
