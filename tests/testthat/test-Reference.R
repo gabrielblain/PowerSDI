8f700994c9a90da8050764b6403003943f694f15
Adam H. Sparks <adamhsparks@icloud.com>
2023-10-22 17:23:45 +0800

    Enhance tests, update values

--- tests/testthat/test-Reference.R
+++ tests/testthat/test-Reference.R
@@ -1,20 +1,13 @@
 test_that("Reference() calculates SPI/SPEI using GEV & HS w/ TS4", {
-  data("refHS")
-
-  expect_warning(Reference(
-    ref = refHS,
-    distr = "GEV",
-    PEMethod = "HS",
-    TS = 4L
-  ),
-  regexp = "The latest quart.month period is not complete")
-
-  expect_warning(ref_gev <- Reference(
-    ref = refHS,
-    distr = "GEV",
-    PEMethod = "HS",
-    TS = 4
-  ), "The latest quart.month period is not complete")
+  expect_warning(
+    ref_gev <- Reference(
+      ref = refHS,
+      distr = "GEV",
+      PEMethod = "HS",
+      TS = 4L
+    ),
+    "The latest quart.month period is not complete"
+  )
   expect_s3_class(ref_gev, "data.frame")
   expect_length(ref_gev, 10)
   expect_equal(nrow(ref_gev), 1435)
@@ -33,18 +26,135 @@ test_that("Reference() calculates SPI/SPEI using GEV & HS w/ TS4", {
       "Categ.SPEI"
     )
   )
+
+  expect_equal(
+    ref_gev$Rain[1:10],
+    c(
+      517.7,
+      530.5,
+      455.5,
+      242.5,
+      202.5,
+      149.3,
+      135.3,
+      347.8,
+      256.8,
+      250.2
+    ),
+    tolerance = 0.1
+  )
+  expect_equal(
+    ref_gev$PE[1:10],
+    c(
+      151.51484229481,
+      149.085625905187,
+      148.730014928848,
+      137.443125147119,
+      127.995698624078,
+      129.712688148129,
+      124.511899737206,
+      115.146547681111,
+      116.458264326486,
+      108.969475304204
+    ),
+    tolerance = 0.1
+  )
+
+  expect_equal(
+    ref_gev$PPE[1:10],
+    c(
+      366.18515770519,
+      381.414374094813,
+      306.769985071152,
+      105.056874852881,
+      74.5043013759225,
+      19.5873118518713,
+      10.7881002627939,
+      232.653452318889,
+      140.341735673514,
+      141.230524695796
+    ),
+    tolerance = 0.1
+  )
+
+  expect_equal(
+    ref_gev$SPI[1:10],
+    c(
+      2.2726620181654,
+      2.27714084882156,
+      1.91776980161342,
+      0.748993763410941,
+      0.434572929905318,
+      -0.116602821250248,
+      -0.1579964385339,
+      1.96308365370141,
+      1.39509585295987,
+      1.6415716285752
+    ),
+    tolerance = 0.1
+  )
+
+  expect_equal(
+    ref_gev$SPEI[1:10],
+    c(
+      2.10571151816844,
+      2.19000429999202,
+      1.77871012111374,
+      0.67594956846407,
+      0.399478705108477,
+      0.0355288934418908,
+      -0.0971844132189002,
+      2.01361366292708,
+      1.55693457747235,
+      1.81488661351313
+    ),
+    tolerance = 0.1
+  )
+
+  expect_equal(
+    ref_gev$Categ.SPI[1:10],
+    c(
+      "ext.wet",
+      "ext.wet",
+      "sev.wet",
+      "Normal",
+      "Normal",
+      "Normal",
+      "Normal",
+      "sev.wet",
+      "mod.wet",
+      "sev.wet"
+    )
+  )
+
+  expect_equal(
+    ref_gev$Categ.SPEI[1:10],
+    c(
+      "ext.wet",
+      "ext.wet",
+      "sev.wet",
+      "Normal",
+      "Normal",
+      "Normal",
+      "Normal",
+      "ext.wet",
+      "sev.wet",
+      "sev.wet"
+    )
+  )
 })
 
 
 test_that("Reference() calculates SPI/SPEI using GLO & HS w/ TS4", {
-  data("refHS")
-
-  expect_warning(ref_glo <- Reference(
-    ref = refHS,
-    distr = "GLO",
-    PEMethod = "HS",
-    TS = 4
-  ), "The latest quart.month period is not complete")
+  expect_warning(
+    ref_glo <- Reference(
+      ref = refHS,
+      distr = "GLO",
+      PEMethod = "HS",
+      TS = 4L
+    ),
+    "The latest quart.month period is not complete"
+  )
 
   expect_s3_class(ref_glo, "data.frame")
   expect_length(ref_glo, 10)
@@ -62,6 +172,122 @@ test_that("Reference() calculates SPI/SPEI using GLO & HS w/ TS4", {
       "SPEI",
       "Categ.SPI",
       "Categ.SPEI"
+    )
+  )
+
+  expect_equal(
+    ref_glo$Rain[1:10],
+    c(
+      517.7,
+      530.5,
+      455.5,
+      242.5,
+      202.5,
+      149.3,
+      135.3,
+      347.8,
+      256.8,
+      250.2
+    ),
+    tolerance = 0.1
+  )
+  expect_equal(
+    ref_glo$PE[1:10],
+    c(
+      151.51484229481,
+      149.085625905187,
+      148.730014928848,
+      137.443125147119,
+      127.995698624078,
+      129.712688148129,
+      124.511899737206,
+      115.146547681111,
+      116.458264326486,
+      108.969475304204
+    ),
+    tolerance = 0.01
+  )
+
+  expect_equal(
+    ref_glo$PPE[1:10],
+    c(
+      366.18515770519,
+      381.414374094813,
+      306.769985071152,
+      105.056874852881,
+      74.5043013759225,
+      19.5873118518713,
+      10.7881002627939,
+      232.653452318889,
+      140.341735673514,
+      141.230524695796
+    ),
+    tolerance = 0.01
+  )
+
+  expect_equal(
+    ref_glo$SPI[1:10],
+    c(
+      2.2726620181654,
+      2.27714084882156,
+      1.91776980161342,
+      0.748993763410941,
+      0.434572929905318,
+      -0.116602821250248,
+      -0.1579964385339,
+      1.96308365370141,
+      1.39509585295987,
+      1.6415716285752
+    ),
+    tolerance = 0.01
+  )
+
+  expect_equal(
+    ref_glo$SPEI[1:10],
+    c(
+      2.0484784193881,
+      2.11276926710058,
+      1.79495229585095,
+      0.756133104534819,
+      0.459409497099322,
+      0.0195320712562312,
+      -0.121174626129315,
+      1.97671118833318,
+      1.58719496809762,
+      1.80750965633644
+    ),
+    tolerance = 0.01
+  )
+
+  expect_equal(
+    ref_glo$Categ.SPI[1:10],
+    c(
+      "ext.wet",
+      "ext.wet",
+      "sev.wet",
+      "Normal",
+      "Normal",
+      "Normal",
+      "Normal",
+      "sev.wet",
+      "mod.wet",
+      "sev.wet"
+    )
+  )
+
+  expect_equal(
+    ref_glo$Categ.SPEI[1:10],
+    c(
+      "ext.wet",
+      "ext.wet",
+      "sev.wet",
+      "Normal",
+      "Normal",
+      "Normal",
+      "Normal",
+      "sev.wet",
+      "sev.wet",
+      "sev.wet"
     )
   )
 })
--- tests/testthat/test-ScientSDI.R
+++ tests/testthat/test-ScientSDI.R
@@ -1,6 +1,5 @@
 
 # in most of these tests, `Good = "no"` because it's faster and is tested 1X
-
 test_that("ScientSDI properly fetches and calculates values TS=1, distr=GEV",
           {
             vcr::use_cassette("ScientSDI", {
@@ -110,88 +109,243 @@ test_that("ScientSDI properly fetches and calculates values, Good='yes'",
                   "Categ.SPEI.PM"
                 )
               )
-              expect_equal(s_sdi[[1]]$Rain[1], 260.76, tolerance = 0.01)
-              expect_equal(s_sdi[[1]]$PE.Harg[1], 174.228016, tolerance = 0.01)
-              expect_equal(s_sdi[[1]]$PE.PM[1], 148.660225, tolerance = 0.01)
-              expect_equal(s_sdi[[1]]$PPE.Harg[1], 86.531984, tolerance = 0.01)
-              expect_equal(s_sdi[[1]]$PPE.PM[1], 112.099775, tolerance = 0.01)
-              expect_equal(s_sdi[[1]]$SPI[1], 0.409543, tolerance = 0.01)
-              expect_equal(s_sdi[[1]]$SPEI.Harg[1], 0.061162, tolerance = 0.01)
-              expect_equal(s_sdi[[1]]$Categ.SPI[1], "Normal")
-              expect_equal(s_sdi[[1]]$Categ.SPEI.Harg[1], "Normal")
-              expect_equal(s_sdi[[1]]$Categ.SPEI.PM[1], "Normal")
+              expect_equal(
+                s_sdi[[1]]$Rain[1:10],
+                c(
+                  260.76,
+                  338.79,
+                  359.57,
+                  336.22,
+                  236.5,
+                  248.75,
+                  198.76,
+                  223.75,
+                  360.42,
+                  245.98
+                ),
+                tolerance = 0.01
+              )
+              expect_equal(
+                s_sdi[[1]]$PE.Harg[1:10],
+                c(
+                  152.659403425914,
+                  145.896009143681,
+                  139.457767681156,
+                  135.433846339862,
+                  119.544849667494,
+                  114.479425623325,
+                  111.612945595566,
+                  106.907680085454,
+                  107.008645749893,
+                  105.666232270404
+                ),
+                tolerance = 0.01
+              )
+              expect_equal(
+                s_sdi[[1]]$PE.PM[1:10],
+                c(
+                  132.034697939064,
+                  123.627657536289,
+                  119.917308569187,
+                  117.341358194535,
+                  103.305687785089,
+                  98.0509744109017,
+                  99.7318359574366,
+                  94.3866880409517,
+                  85.6493596073208,
+                  90.9219118138176
+                ),
+                tolerance = 0.01
+              )
+              expect_equal(
+                s_sdi[[1]]$PPE.Harg[1:10],
+                c(
+                  108.100596574086,
+                  192.893990856319,
+                  220.112232318844,
+                  200.786153660138,
+                  116.955150332506,
+                  134.270574376675,
+                  87.1470544044343,
+                  116.842319914546,
+                  253.411354250107,
+                  140.313767729596
+                ),
+                tolerance = 0.01
+              )
+              expect_equal(
+                s_sdi[[1]]$PPE.PM[1:10],
+                c(
+                  128.725302060936,
+                  215.162342463711,
+                  239.652691430812,
+                  218.878641805465,
+                  133.194312214911,
+                  150.699025589098,
+                  99.0281640425634,
+                  129.363311959048,
+                  274.770640392679,
+                  155.058088186182
+                ),
+                tolerance = 0.01
+              )
+              expect_equal(
+                s_sdi[[1]]$SPI[1:10],
+                c(
+                  0.409543204634262,
+                  1.32329551119963,
+                  1.73669251746959,
+                  1.4562523684948,
+                  0.9173381440806,
+                  1.35163443304433,
+                  1.05139101697662,
+                  1.56390357609342,
+                  2.99975144511263,
+                  2.29194297954978
+                ),
+                tolerance = 0.01
+              )
+              expect_equal(
+                s_sdi[[1]]$SPEI.Harg[1:10],
+                c(
+                  0.113858299490916,
+                  1.2456232844912,
+                  1.94152836951902,
+                  1.43833614411158,
+                  0.875021064254541,
+                  1.3598868087475,
+                  1.04701422560855,
+                  1.67950700819748,
+                  2.64231692991216,
+                  2.8474815696763
+                ),
+                tolerance = 0.01
+              )
+              expect_equal(
+                s_sdi[[1]]$Categ.SPI[1:10],
+                c(
+                  "Normal",
+                  "mod.wet",
+                  "sev.wet",
+                  "mod.wet",
+                  "Normal",
+                  "mod.wet",
+                  "mod.wet",
+                  "sev.wet",
+                  "ext.wet",
+                  "ext.wet"
+                )
+              )
+              expect_equal(
+                s_sdi[[1]]$Categ.SPEI.Harg[1:10],
+                c(
+                  "Normal",
+                  "mod.wet",
+                  "sev.wet",
+                  "mod.wet",
+                  "Normal",
+                  "mod.wet",
+                  "mod.wet",
+                  "sev.wet",
+                  "ext.wet",
+                  "ext.wet"
+                )
+              )
+              expect_equal(
+                s_sdi[[1]]$Categ.SPEI.PM[1:10],
+                c(
+                  "Normal",
+                  "mod.wet",
+                  "ext.wet",
+                  "mod.wet",
+                  "Normal",
+                  "mod.wet",
+                  "mod.wet",
+                  "sev.wet",
+                  "ext.wet",
+                  "ext.wet"
+                )
+              )
             })
           })
 
 ## expect messages for removed rows ----
-
-expect_message(
-  s_sdi <- ScientSDI(
-    lon = -48.11,
-    lat = -24.81,
-    start.date = "1991-01-01",
-    end.date = "2022-12-31",
-    TS = 1,
-    Good = "no",
-    distr = "GEV",
-    PEUplim = 45
-  ),
-  "above limit: 48, 52, 96, 336"
-)
-expect_length(s_sdi, 2)
-expect_equal(s_sdi[[1]]$PE.PM[1], 30.120835, tolerance = 0.01)
+test_that("ScientSDI emits a message and removes rows above the limit", {
+  expect_message(
+    s_sdi <- ScientSDI(
+      lon = -48.11,
+      lat = -24.81,
+      start.date = "1991-01-01",
+      end.date = "2022-12-31",
+      TS = 1,
+      Good = "no",
+      distr = "GEV",
+      PEUplim = 45
+    ),
+    regexp = "above limit: 1108, 1248"
+  )
+  expect_length(s_sdi, 2)
+  expect_equal(s_sdi[[1]]$PE.PM[1], 28.37215, tolerance = 0.01)
+})
 
 ## Check user inputs, expect errors ----
-
-expect_error(
-  ScientSDI(
-    lon = -47.3,
-    lat = -22.87,
-    start.date = "2015-01-01",
-    end.date = "2022-12-31",
-    TS = 1,
-    Good = "maybe"
+test_that("ScientSDI errors with an invalid entry for 'Good'", {
+  expect_error(
+    ScientSDI(
+      lon = -47.3,
+      lat = -22.87,
+      start.date = "2015-01-01",
+      end.date = "2022-12-31",
+      TS = 1,
+      Good = "maybe"
+    )
   )
-)
+})
 
-expect_error(
-  s_sdi <- ScientSDI(
-    lon = -48.11,
-    lat = -24.81,
-    start.date = "1991-01-01",
-    end.date = "2022-12-31",
-    TS = 1,
-    Good = "yes",
-    distr = "GEV",
-    PELowlim = 100
-  ),
-  "There are not enough rows in the data"
-)
+test_that("ScientSDIerros with not enough data to run", {
+  expect_error(
+    s_sdi <- ScientSDI(
+      lon = -48.11,
+      lat = -24.81,
+      start.date = "1991-01-01",
+      end.date = "2022-12-31",
+      TS = 1,
+      Good = "yes",
+      distr = "GEV",
+      PELowlim = 100
+    ),
+    "There are not enough rows in the data"
+  )
+})
 
-expect_error(
-  s_sdi <- ScientSDI(
-    lon = -48.11,
-    lat = -24.81,
-    start.date = "1991-01-01",
-    end.date = "2022-12-31",
-    TS = 1,
-    Good = "yes",
-    distr = "GEV",
-    PELowlim = "onehundred"
-  ),
-  "Please, provide appropriate numerical values for `RainUplim` or"
-)
+test_that("ScientSDI errors with invalid values for 'Rainuplim'", {
+  expect_error(
+    s_sdi <- ScientSDI(
+      lon = -48.11,
+      lat = -24.81,
+      start.date = "1991-01-01",
+      end.date = "2022-12-31",
+      TS = 1,
+      Good = "yes",
+      distr = "GEV",
+      PELowlim = "onehundred"
+    ),
+    "Please, provide appropriate numerical values for `RainUplim` or"
+  )
+})
 
-expect_error(
-  s_sdi <- ScientSDI(
-    lon = -48.11,
-    lat = -24.81,
-    start.date = "1991-01-01",
-    end.date = "1991-01-02",
-    TS = 1,
-    Good = "yes",
-    distr = "GEV",
-    PELowlim = 100
-  ),
-  "Please select a longer period between start.date and end.date."
-)
+test_that("ScientSDI errors when dates are too close together", {
+  expect_error(
+    s_sdi <- ScientSDI(
+      lon = -48.11,
+      lat = -24.81,
+      start.date = "1991-01-01",
+      end.date = "1991-01-02",
+      TS = 1,
+      Good = "yes",
+      distr = "GEV",
+      PELowlim = 100
+    ),
+    "Please select a longer period between start.date and end.date."
+  )
+})
