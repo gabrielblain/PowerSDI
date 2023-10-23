000b6c61154e2e4f1cb1fedb3fa144fc2399f46a
Adam H. Sparks <adamhsparks@icloud.com>
2023-10-23 19:14:38 +0800

    Brings test coverage to ~97% now

--- tests/testthat/test-Reference.R
+++ tests/testthat/test-Reference.R
@@ -1,3 +1,6 @@
+
+## Method = "HS" ----
+
 test_that("Reference() calculates SPI/SPEI using GEV & HS w/ TS4", {
   expect_warning(
     ref_gev <- Reference(
@@ -57,7 +60,7 @@ test_that("Reference() calculates SPI/SPEI using GEV & HS w/ TS4", {
       116.458264326486,
       108.969475304204
     ),
-    tolerance = 0.1
+    tolerance = 0.01
   )
 
   expect_equal(
@@ -74,7 +77,7 @@ test_that("Reference() calculates SPI/SPEI using GEV & HS w/ TS4", {
       140.341735673514,
       141.230524695796
     ),
-    tolerance = 0.1
+    tolerance = 0.01
   )
 
   expect_equal(
@@ -91,7 +94,7 @@ test_that("Reference() calculates SPI/SPEI using GEV & HS w/ TS4", {
       1.39509585295987,
       1.6415716285752
     ),
-    tolerance = 0.1
+    tolerance = 0.01
   )
 
   expect_equal(
@@ -108,7 +111,7 @@ test_that("Reference() calculates SPI/SPEI using GEV & HS w/ TS4", {
       1.55693457747235,
       1.81488661351313
     ),
-    tolerance = 0.1
+    tolerance = 0.01
   )
 
   expect_equal(
@@ -292,6 +295,297 @@ test_that("Reference() calculates SPI/SPEI using GLO & HS w/ TS4", {
   )
 })
 
+## Method = "PM" -----
+
+test_that("Reference() calculates SPI/SPEI using GEV & PM w/ TS4", {
+  ref_gev <- Reference(
+    ref = refPM,
+    distr = "GEV",
+    PEMethod = "PM",
+    TS = 4L
+  )
+
+  expect_s3_class(ref_gev, "data.frame")
+  expect_length(ref_gev, 10)
+  expect_equal(nrow(ref_gev), 1435)
+  expect_named(
+    ref_gev,
+    c(
+      "Year",
+      "Month",
+      "quart.month",
+      "Rain",
+      "PE",
+      "PPE",
+      "SPI",
+      "SPEI",
+      "Categ.SPI",
+      "Categ.SPEI"
+    )
+  )
+
+  expect_equal(
+    ref_gev$Rain[1:10],
+    c(
+      352.37,
+      354.43,
+      333.99,
+      223.02,
+      224.44,
+      196.08,
+      220.88,
+      380.11,
+      285,
+      253.48
+    ),
+    tolerance = 0.01
+  )
+  expect_equal(
+    ref_gev$PE[1:10],
+    c(
+      129.680970873538,
+      125.817221761724,
+      123.368573047977,
+      111.274831585327,
+      105.795861324817,
+      107.611289852481,
+      102.423439585401,
+      90.6403414919855,
+      96.4932881727808,
+      92.5905065013557
+    ),
+    tolerance = 0.01
+  )
+
+  expect_equal(
+    ref_gev$PPE[1:10],
+    c(
+      222.689029126462,
+      228.612778238276,
+      210.621426952023,
+      111.745168414673,
+      118.644138675183,
+      88.4687101475186,
+      118.456560414599,
+      289.469658508015,
+      188.506711827219,
+      160.889493498644
+    ),
+    tolerance = 0.01
+  )
+
+  expect_equal(
+    ref_gev$SPI[1:10],
+    c(
+      1.46519296007417,
+      1.61278839499981,
+      1.39053741462298,
+      0.573924325713957,
+      0.921708775264062,
+      0.699728370335599,
+      1.36209784511568,
+      2.99975144511263,
+      2.51167741199298,
+      2.37026724613154
+    ),
+    tolerance = 0.01
+  )
+
+  expect_equal(
+    ref_gev$SPEI[1:10],
+    c(
+      1.50931225566312,
+      1.80214589657084,
+      1.36579315604598,
+      0.546724767747658,
+      0.946328515139361,
+      0.814668569844952,
+      1.37326596082147,
+      2.44686909226193,
+      2.26637470415321,
+      2.22142968705348
+    ),
+    tolerance = 0.01
+  )
+
+  expect_equal(
+    ref_gev$Categ.SPI[1:10],
+    c(
+      "mod.wet",
+      "sev.wet",
+      "mod.wet",
+      "Normal",
+      "Normal",
+      "Normal",
+      "mod.wet",
+      "ext.wet",
+      "ext.wet",
+      "ext.wet"
+    )
+  )
+
+  expect_equal(
+    ref_gev$Categ.SPEI[1:10],
+    c(
+      "sev.wet",
+      "sev.wet",
+      "mod.wet",
+      "Normal",
+      "Normal",
+      "Normal",
+      "mod.wet",
+      "ext.wet",
+      "ext.wet",
+      "ext.wet"
+    )
+  )
+})
+
+
+test_that("Reference() calculates SPI/SPEI using GLO & HS w/ TS4", {
+  ref_glo <- Reference(
+    ref = refPM,
+    distr = "GLO",
+    PEMethod = "PM",
+    TS = 4L
+  )
+
+  expect_s3_class(ref_glo, "data.frame")
+  expect_length(ref_glo, 10)
+  expect_equal(nrow(ref_glo), 1435)
+  expect_named(
+    ref_glo,
+    c(
+      "Year",
+      "Month",
+      "quart.month",
+      "Rain",
+      "PE",
+      "PPE",
+      "SPI",
+      "SPEI",
+      "Categ.SPI",
+      "Categ.SPEI"
+    )
+  )
+
+  expect_equal(
+    ref_glo$Rain[1:10],
+    c(
+      352.37,
+      354.43,
+      333.99,
+      223.02,
+      224.44,
+      196.08,
+      220.88,
+      380.11,
+      285,
+      253.48
+    ),
+    tolerance = 0.01
+  )
+  expect_equal(
+    ref_glo$PE[1:10],
+    c(
+      129.680970873538,
+      125.817221761724,
+      123.368573047977,
+      111.274831585327,
+      105.795861324817,
+      107.611289852481,
+      102.423439585401,
+      90.6403414919855,
+      96.4932881727808,
+      92.5905065013557
+    ),
+    tolerance = 0.01
+  )
+
+  expect_equal(
+    ref_glo$PPE[1:10],
+    c(
+      222.689029126462,
+      228.612778238276,
+      210.621426952023,
+      111.745168414673,
+      118.644138675183,
+      88.4687101475186,
+      118.456560414599,
+      289.469658508015,
+      188.506711827219,
+      160.889493498644
+    ),
+    tolerance = 0.01
+  )
+
+  expect_equal(
+    ref_glo$SPI[1:10],
+    c(
+      1.46519296007417,
+      1.61278839499981,
+      1.39053741462298,
+      0.573924325713957,
+      0.921708775264062,
+      0.699728370335599,
+      1.36209784511568,
+      2.99975144511263,
+      2.51167741199298,
+      2.37026724613154
+    ),
+    tolerance = 0.01
+  )
+
+  expect_equal(
+    ref_glo$SPEI[1:10],
+    c(
+      1.5127259508262,
+      1.72374724914942,
+      1.41589158933947,
+      0.609941429365168,
+      1.03344120455704,
+      0.872954823158599,
+      1.42429241279852,
+      2.36621176787509,
+      2.17824619265434,
+      2.13752609645037
+    ),
+    tolerance = 0.01
+  )
+
+  expect_equal(
+    ref_glo$Categ.SPI[1:10],
+    c(
+      "mod.wet",
+      "sev.wet",
+      "mod.wet",
+      "Normal",
+      "Normal",
+      "Normal",
+      "mod.wet",
+      "ext.wet",
+      "ext.wet",
+      "ext.wet"
+    )
+  )
+
+  expect_equal(
+    ref_glo$Categ.SPEI[1:10],
+    c(
+      "sev.wet",
+      "sev.wet",
+      "mod.wet",
+      "Normal",
+      "mod.wet",
+      "Normal",
+      "mod.wet",
+      "ext.wet",
+      "ext.wet",
+      "ext.wet"
+    )
+  )
+})
+
 # test user input checks ----
 
 test_that("Reference() stops on invalid PEMethod", {
@@ -338,4 +632,26 @@ test_that("Reference() stops on invalid TS value", {
     TS = 100
   ),
   regexp = "TS must be a whole value ranging between 1 and 96*")
+})
+
+test_that("Reference() errors w/ 'HS' and cols != 8 in ref", {
+  expect_error(ref_glo <- Reference(
+    ref = refPM,
+    distr = "GLO",
+    PEMethod = "HS",
+    TS = 4L
+  ),
+  "It should be 8.")
+})
+
+test_that("Reference() errors w/ 'PM' and cols != 11 in ref", {
+  expect_error(
+    ref_glo <- Reference(
+      ref = refHS,
+      distr = "GLO",
+      PEMethod = "PM",
+      TS = 4L
+    ),
+    "It should be 11."
+  )
 })
