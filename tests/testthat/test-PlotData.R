d927f37289892e9fd88769b510364aa1cbd65533
Adam H. Sparks <adamhsparks@icloud.com>
2023-11-11 12:32:04 +0800

    Use vcr for testing plot data

--- tests/testthat/test-PlotData.R
+++ tests/testthat/test-PlotData.R
@@ -1,12 +1,15 @@
+
 test_that("PlotData creates proper plots", {
   skip_if_offline()
-  PlotData_a <- PlotData(
-    lon = -47.3,
-    lat = -22.87,
-    start.date = "2021-12-28",
-    end.date = "2022-12-31"
-  )
-  vdiffr::expect_doppelganger("disp-PlotData-a", PlotData_a)
+  vcr::use_cassette("PlotData", {
+    PlotData_a <- PlotData(
+      lon = -47.3,
+      lat = -22.87,
+      start.date = "2021-12-28",
+      end.date = "2022-12-31"
+    )
+    vdiffr::expect_doppelganger("disp-PlotData-a", PlotData_a)
+  })
 })
 
 test_that("PlotData stops if not enough time between dates", {
