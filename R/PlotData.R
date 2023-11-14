e58fc6bc1e146c50aef7c4c662b878fb1b83872e
Adam H. Sparks <adamhsparks@icloud.com>
2023-11-14 18:46:48 +0800

    More simplifying of duplicated values and fns

--- R/PlotData.R
+++ R/PlotData.R
@@ -44,71 +44,25 @@ PlotData <- function(lon, lat, start.date, end.date) {
     )
   }
 
-  start.user.day <-
-    as.numeric(format(start.date.user, format = "%d"))
-  start.month <-
-    as.numeric(format(start.date.user, format = "%m"))
-  start.year <-
-    as.numeric(format(start.date.user, format = "%Y"))
+  start.year <- lubridate::year(start.date.user)
+  start.month <- lubridate::month(start.date.user)
+  final.year <- lubridate::year(end.date.user)
+  final.month <- lubridate::month(end.date.user)
+  final.day <- lubridate::day(end.date.user)
+  start.week <-
+    find.week.int(lubridate::day(start.date.user)) # see internal_functions.R
 
-  start.week <- find.week.int(start.user.day) # see internal_functions.R
-  dif <- calculate.dif(start.week, start.user.day) # see internal_functions.R
-
-  start.date.protocal <- start.date.user - dif
   message(
     "Just a sec. ",
     "Downloading NASA POWER data and calculating the others parameters.")
-  sse_i <-
-    get_sdi_power_data(lon, lat, start.date.user, end.date.user)
-  decli <-
-    23.45 * sin((360 * (sse_i$DOY - 80) / 365) * (0.01745329))
-  lat.rad <- lat * (0.01745329)
-  decli.rad <- decli * (0.01745329)
-  hn.rad <- (acos(tan(decli.rad) * -tan(lat.rad)))
-  hn.deg <- hn.rad * (180 / pi)
-  N <- (2 * hn.deg) / 15
-
-  dist.terra.sol <-
-    1 + (0.033 * cos((0.01745329) * (sse_i$DOY * (0.9863014))))
-
-  Ra <-
-    (37.6 * (dist.terra.sol ^ 2)) *
-    ((0.01745329) *
-       hn.deg * sin(lat.rad) * sin(decli.rad) +
-       (cos(lat.rad) * cos(decli.rad) * sin(hn.rad)))
-
-  ####   Hargreaves&Samani
-  ETP.harg.daily <-
-    0.0023 * (Ra * 0.4081633) * (sse_i$T2M_MAX - sse_i$T2M_MIN) ^ 0.5 *
-    (sse_i$T2M + 17.8)
-
-  ###    Penman- Monteith-FAO
-  es <- 0.6108 * exp((17.27 * sse_i$T2M) / (sse_i$T2M + 273.3))
-  ea <- (sse_i$RH2M * es) / 100
-  slope.pressure <- (4098 * es) / ((sse_i$T2M + 237.3) ^ 2)
-  Q0.ajust <- 0.75 * Ra
-
-  Rn <-
-    (1 - 0.2) * sse_i$ALLSKY_SFC_SW_DWN -
-    (1.35 * (sse_i$ALLSKY_SFC_SW_DWN / Q0.ajust) - 0.35) *
-    (0.35 - (0.14 * sqrt(ea))) * (0.0000000567) *
-    (((sse_i$T2M ^ 4) + (sse_i$T2M_MIN ^ 4)) / 2)
-
-  ETP.pm.daily <-
-    (0.408 * slope.pressure * (Rn - 0.8) + 0.063 * (900 / (sse_i$T2M + 273)) *
-       sse_i$WS2M * (es - ea)) /
-    (slope.pressure + 0.063 * (1 + 0.34 * sse_i$WS2M))
-
-  sse_i <- cbind(sse_i, ETP.harg.daily, ETP.pm.daily)
-  n.tot <- length(sse_i[, 1])
-  final.year <- sse_i$YEAR[n.tot]
-  initial.year <- sse_i$YEAR[1]
-  final.month <- sse_i$MM[n.tot]
-  final.day <- sse_i$DD[n.tot]
+  sse_i <- daily.ETP.wrapper(lon,
+                             lat,
+                             start.date.user,
+                             end.date.user)
 
   # see internal_functions.R for `find.week.int()`
   final.week <- find.week.int(final.day)
-  n.years <- 1 + (final.year - initial.year)
+  n.years <- 1 + (final.year - start.year)
   total.nweeks <- 48 * n.years
   a <- 1
   b <- 2
